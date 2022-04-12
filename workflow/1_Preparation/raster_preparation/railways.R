c51fab6f11c688823adeda7743f82b3622f2e77f
jannebor <74177785+jannebor@users.noreply.github.com>
2022-04-12 08:58:57 +0200

    preparation

---
+++ workflow/1_Preparation/raster_preparation/railways.R
@@ -0,0 +1,26 @@
+library(raster)
+
+#railways: https://doi.org/10.1038/sdata.2016.67
+rails <- raster("Railways.tif")
+
+# generate reference raster
+c1 <- raster()
+res(c1) <- 0.5
+crs(c1) <- CRS("+proj=longlat +datum=WGS84")
+
+rails <- projectRaster(rails, crs=CRS("+proj=longlat +datum=WGS84"))
+
+rails[!is.na(rails)]<-1
+rails[is.na(rails)]<-0
+
+# aggregate by averaging
+rails <- aggregate(x = rails, fact=res(c1)[1]/res(rails)[1], fun=mean)
+  
+# snap to reference raster
+rails<-resample(rails, c1, method="bilinear")
+rails<-extend(rails, extent(c1))
+rails<-crop(rails, c1)
+  
+writeRaster(rails, filename = "Railways_WGS84_binary_res2.tif", overwrite = T)
+
+
